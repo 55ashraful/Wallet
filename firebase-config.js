c4b7978a4c009a46a80375cd4647ee107ae4b588
55ashraful <ashrafulhaqedulon@gmail.com>
2026-09-04 12:45:22 +0600

    Create firebase-config.js with credentials
    Added Firebase configuration and IMGBB API key.

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase & API Credentials File
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC3tgTH-Cp16LrJzAjgisfgvBTibRF0T4g",
+  authDomain: "studio-ar1ak.firebaseapp.com",
+  databaseURL: "https://studio-ar1ak-default-rtdb.firebaseio.com",
+  projectId: "studio-ar1ak",
+  storageBucket: "studio-ar1ak.firebasestorage.app",
+  messagingSenderId: "77389723660",
+  appId: "1:77389723660:web:2e35437b147041db02d629"
+};
+
+export const IMGBB_API_KEY = "d617dab9d2117228e38549791d42104a";
